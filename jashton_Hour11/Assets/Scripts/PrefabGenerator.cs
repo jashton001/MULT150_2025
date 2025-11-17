a7d9fbabd96aeaadb0824d04b0f66ce8cafb5157
Joe Ashton <jashton001@student.sdccd.edu>
2025-11-16 16:54:48 -0800

    Hour 11 Part 2

--- jashton_Hour11/Assets/Scripts/PrefabGenerator.cs
+++ jashton_Hour11/Assets/Scripts/PrefabGenerator.cs
@@ -4,25 +4,39 @@ using UnityEngine;
 
 public class PrefabGenerator : MonoBehaviour
 {
+    public GameObject streetLampPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        float spacing = 5f;
+
+        Vector3 spawnPosition = transform.position;
+
+        for (int i = 0; i < 10; i++)
+        {
+            spawnPosition.x = transform.position.x + (i * spacing);
+
+            Instantiate(streetLampPrefab, spawnPosition, Quaternion.identity); 
+        }
     }
 
-    public GameObject prefab; 
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.B))
         {
-            Instantiate(prefab);
+            Instantiate(streetLampPrefab, transform.position, Quaternion.identity);
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(prefab, transform.position, transform.rotation);
+            GameObject[] lamps = GameObject.FindGameObjectsWithTag("Lamp");
+            foreach (GameObject lamp in lamps)
+            {
+                Destroy(lamp);
+            }
         }
     }
 }
