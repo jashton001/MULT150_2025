6200a8f58254199558bf283f9fd033853dbc21db
Joe Ashton <jashton001@student.sdccd.edu>
2025-11-21 16:36:45 -0800

    Hour 14 Part 1

---
+++ jashton_Hour14and16/Assets/UserInterfaces.cs
@@ -0,0 +1,15 @@
+/* UserInterface.cs Description
+ * 
+ * Throughout the project, I made a few changes from the exercise to make it look presentable.
+ * 
+ * - I added a picture of a the beach with a nice, transparent blu-ish color in the background.
+ * 
+ * - I tried to change the text of the description of the sub-heading text, the description is a random message, I 
+ *   created to make it funny at least.
+ *   
+ * - I changed the colors of the text for the headings, so it doesn't look basic!
+ * 
+ * - The button has also been customized as well, the normal color is much of a teal color. When you hover over it, it's a blue color,
+ *   then when you click it, it turns green! Once you click the button, it says "Loading...".
+ *   
+ */
