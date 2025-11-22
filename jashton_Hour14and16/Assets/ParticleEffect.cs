97de7be40d33a7599f62831ee969d29e9a6dadfd
Joe Ashton <jashton001@student.sdccd.edu>
2025-11-22 15:57:16 -0800

    Hour14and16 Part 2

---
+++ jashton_Hour14and16/Assets/ParticleEffect.cs
@@ -0,0 +1,32 @@
+/* 
+ * Particle Effect Modifications
+ * 
+ * - Cube 
+ *     - The color of the cube is teal, I wanted to give this my all onto this one. I was very interested in seeing
+ *       what I could possibly do with this to make it my own! With the particles spawning around the object, and it
+ *       looks like it is leveling up! 
+ *       
+ *   1. Duration - 3.0
+ *   2. Start Lifetime - 3.0 
+ *   3. Start Speed - 2.4 
+ *   4. Simulation Speed - 1.15 
+ *   5. Emission 
+ *      - Rate Over Time = 5
+ *      - Rate Over Distance = 0
+ *      
+ * - Cylinder 
+ *      - I modified the particle system to the cylinder to make it look similar to the video from the assignment.
+ *        I tried to make it look pretty nice, and to make sure that particles match the color of the object! 
+ *    
+ *   1. Duration - 6.15
+ *   2. Start Lifetime - 1.0
+ *   3. Start Speed - 3
+ *   4. Simulation Speed - 1.93
+ *   5. Emission
+ *      - Rate Over Time = 25
+ *      - Rate Over Distance = 5
+ *   6. Color by Speed
+ *   7. Color over Lifetime
+ *   8. Collision
+ *   
+ */
