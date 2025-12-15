5e317731f31d3a274957d6ea54f67e01d096a26a
Joe Ashton <jashton001@student.sdccd.edu>
2025-12-14 20:08:25 -0800

    Hour 20 - Gauntlet Runner!

---
+++ jashton_Hour20/Assets/Scripts/GameCritic.cs
@@ -0,0 +1,30 @@
+/* Hour 20 - Gauntlet Runner Game
+ * 
+ * 1. Is the game too easy or hard?
+ * - I would say this game is too easy, the amount of obstacles aren't appearing as much, and we made the objects move 
+ *   slowly for the player to have time to avoid them.
+ *   
+ * 2. What changes would you do to make it easier or harder?
+ * - The changes I would make is making things a bit faster so that the game can be a little more challenging. 
+ * 
+ * 3. What would make the game more appealing?
+ * - I would say just customizing the environment a little more, so it doesn't have to be only a dark environment. Or
+ *   adding stuff in the background for a little more creativity.
+ *   
+ * 4. What parts of the game are fun? 
+ * - Honestly, this game is somewhat fun, there's nothing you can really do besides waiting until the time runs out with additional objects.
+ * 
+ * 5. What parts of the game are tedious?
+ * - I would say the amount of time is given from the book, and the speed leveling throughout the game. The player and the ground moves very slow.
+ * 
+ * 
+ * Changes: 
+ * 1. I somewhat changed the color of the obstacle, but I created an among us character as a statue just because of adding multiple shapes into one,
+ *    and to make it look creative!
+ *    
+ * 2. I also added more time into the game, the game before was very quick so I wanted to extend it. 
+ * 
+ * 3. The powerup shape is changed into a different shape that I created! It looks somewhat cool but I also want to add multiple shape to make it look like an object.
+ *    - The particle system pattern changed as well
+ *    - the color of the shape is blue, with a purple colored particle around it!
+ */
